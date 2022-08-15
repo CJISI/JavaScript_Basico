54a015b66f0ae93784cbbb3d8f4c37fcfce81af6
Cristian Jimenez <cristian.jisi@outlook.es>
2022-08-15 23:46:52 +0200

    Ejercicio 5

---
+++ Ejercicio_5/Ejercicio_5.js
@@ -0,0 +1,11 @@
+let altura_cm = 171;
+let altura_m = 1.71;
+let peso = 85.0;
+
+let altura = Math.round(altura_m)
+console.log(altura)
+let peso_red = Math.floor(peso)
+console.log(peso_red)
+
+let max_valor = (Number.MAX_VALUE + 1) === Number.MAX_VALUE;
+console.log(max_valor)
