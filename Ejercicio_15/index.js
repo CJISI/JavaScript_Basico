8bd452d5a6e9c336e320e2706914dd95f009d965
Cristian Jimenez <cristian.jisi@outlook.es>
2022-09-23 08:53:43 +0200

    commit

---
+++ Ejercicio_15/index.js
@@ -0,0 +1,13 @@
+sessionStorage.setItem("hola", "holquetal")
+const nombre = 'Cristian'
+const apellido = 'Jimenez'
+const persona = {
+    nombre: nombre,
+    apellido: apellido
+}
+
+
+// localStorage.setItem('persona', JSON.stringify(persona))
+// const date = new Date();
+// date.setTime(date.getTime() + (30 * 1000))
+// document.cookie = `Cristian=${persona};expires=` + date.toGMTString();
